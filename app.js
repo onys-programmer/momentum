2f232f02d30c6974b940e2a98c83a9ae8ad989e8
onys <thkim2017@gmail.com>
2021-08-03 23:44:20 +0900

    Activate click button
    Implement onLoginBtnClick by addEventListener

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+function onLoginBtnClick() {
+  console.log("click!")
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
