3a9a74444ddd286ff52d0a810e12f802fe37fe02
onys <thkim2017@gmail.com>
2021-08-05 21:54:39 +0900

    Make print input value when logIn button is clicked

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("click!")
+  console.log(loginInput.value);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
