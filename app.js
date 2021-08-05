ebe64d321c64f7d49e8726e5cff57eb78ed15268
onys <thkim2017@gmail.com>
2021-08-05 22:11:16 +0900

    Change click event to submit event
    In order to enable submit by pressing enter
    In order to make browser check the validity of the login form

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInputValue;
-
-  if(username === "") {
-    alert("Please write your name");
-  } else if(username.length > 15) {
-    alert("Your name is too long");
-  } else {
-    console.log(username)
-  }
+function onLoginSubmit() {
+  const username = loginInput.value;
+  console.log(username)
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
